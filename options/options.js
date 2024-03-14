06d76d827fcb9851bb866e4c2e6ec91296ca8057
Crowdbotics <team@crowdbotics.com>
2024-03-14 17:28:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tydemoapp-47052",
+  url: "https://tydemoapp-47052.botics.co",
+  api: "https://tydemoapp-47052.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
